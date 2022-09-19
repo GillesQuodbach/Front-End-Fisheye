6f8c49ab2ef769e85fdafc4decd252f4b434d241
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-09-19 16:37:37 +0200

    Réorganisation fichers

--- scripts/factories/mediaFactory.js
+++ scripts/factories/mediaFactory.js
@@ -1,40 +1,49 @@
-function photographerFactory(data) {
-    const { name, id, portrait, city, country, tagline, price } = data;
-    const picture = `assets/photographers/${portrait}`;
-    //Création de la carte de chaque photographe
-    function getUserCardDOM() {
-        // Cards container
-        const article = document.createElement( 'article' );
-        article.setAttribute("id", id);
-        article.setAttribute('class', 'cardsLink');
-        //Card image
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        img.className = "profil_image";
-        img.setAttribute("alt", `Photo de profil de ${name}`);
-        // Photographer name
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        h2.className = "name";
-        // Photographers living place
-        const livingPlace = document.createElement('p');
-        livingPlace.innerHTML =`${city}, ${country}`;
-        livingPlace.className = "living-place";
-        // Photographers tagline
-        const tag = document.createElement('p');
-        tag.textContent = tagline;
-        tag.className = "tagline"
-        // Photographers price
-        const cost = document.createElement('p');
-        cost.textContent = `${price}€/jour`;
-        cost.className = "price";
-        //Création de la card
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(livingPlace);
-        article.appendChild(tag);
-        article.appendChild(cost);
-        return (article); //Retourne les infos dans les cards
-    }
-    return { name, picture, id, getUserCardDOM}
-    }
+// PAGE QUI GERE LA GALERIE DES PHOTOGRAPHES
+
+//!MANQUE FACTORY
+async function getMedia() {
+    // Récupération des données du fichier JSON
+    const response = await fetch(json_url3);
+    //Conversion des datas en JSON
+    const data = await response.json();
+    //Affichage du tableau des photographes
+   //Lien vers le fichier JSON
+    console.log(data); //Contenu complet du JSON
+    const mediaData = data.media;
+    console.log(mediaData); //Affiche la partie photographers du JSON
+    //Récupération de l'ID de la page
+    const queryString_url_id = window.location.search;
+    const urlSearchParams = new URLSearchParams(queryString_url_id);
+    const id = urlSearchParams.get("id");
+    console.log(id); //Affiche l'id du photographe selectionné (243)
+    //Affiche la liste par ID
+    const mediaSortedById = {}; 
+    for (let i = 0, len = mediaData.length; i < len; i++) { 
+        mediaSortedById[mediaData[i].id] = mediaData[i]; }
+        console.log(mediaSortedById); 
+        console.log(mediaSortedById[id]); //Affiche l'objet avec l'id de la page
+let mediaArray = data.media; //Tableau des donnés des photographes
+let length = mediaArray.length; //Longueur du tableau
+//Boucle dans le tableau
+    for (let i = 0; i < length; i++){
+        //Affichage du tableau de données des photographes
+        console.log(mediaArray[i])}
+        console.log({media:mediaSortedById[id]})
+//Retourne le tableau photographers seulement une fois
+return ({media:mediaSortedById[id]}) //retourne bien l'objet avec l'id correspondant
+}
+
+//Affichage des données des photographes
+async function displayData(media) {
+    //Selection de la photograph_header de l'index.html (section entière)
+    const photographersSection = document.querySelector(".photograph-header");
+        const photographerModel = mediaFactory(media);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+}
+async function init() {
+    // Récupère les datas des photographes
+    const  media = await getMedia();
+    await displayData(media);
+}
+init(); //RETOURNE UNE PROMESSE EN ATTENTE
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,46 @@
 //Mettre le code JavaScript lié à la page photographer.html
+function photographerFactory(data) {
+    const { name, id, portrait, city, country, tagline, price } = data;
+    const picture = `assets/photographers/${portrait}`;
+    //Création de la carte de chaque photographe
+    function getUserCardDOM() {
+        // Cards container
+        const article = document.createElement( 'article' );
+        article.setAttribute("id", id);
+        //Card image
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture)
+        img.className = "detail_profile_image";
+        img.setAttribute("alt", `Photo de profil de ${name}`);
+        // Photographer name
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = name;
+        h2.className = "name";
+        // Photographers living place
+        const livingPlace = document.createElement('p');
+        livingPlace.innerHTML =`${city}, ${country}`;
+        livingPlace.className = "living-place";
+        // Photographers tagline
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        tag.className = "tagline"
+        // Photographers price
+        const cost = document.createElement('p');
+        cost.textContent = `${price}€/jour`;
+        cost.className = "price";
+        //Création de la card
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(livingPlace);
+        article.appendChild(tag);
+        article.appendChild(cost);
+        return (article); //Retourne les infos dans les cards
+    }
+    return { name, picture, id, getUserCardDOM}
+    }
+
+
+
 const json_url = "data/photographers.json"
 
 async function getPhotographers() {
