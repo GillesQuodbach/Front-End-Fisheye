a94713d8670024d5616859359bc7a6d766e9d875
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-09-20 10:00:15 +0200

    Récupération des photos et vidéos grace a l'ID OK

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,37 +3,35 @@
 //==================================
 const json_url = "data/photographers.json"
 
+//!Récupération des données voulues (Profil des photographes)
+
 async function getPhotographers() {
-    // Récupération des données du fichier JSON
+    //!Récupération de l'ID de la page
+    const queryString_url_id = window.location.search;
+    const urlSearchParams = new URLSearchParams(queryString_url_id);
+    const id = urlSearchParams.get("id");
+    console.log(id); //Affiche l'id du photographe selectionné (243)
+
+    //!Récupération des données du fichier JSON
     const response = await fetch(json_url);
     //Conversion des datas en JSON
     const data = await response.json();
     //Affichage du tableau des photographes
    //Lien vers le fichier JSON
-    console.log(data); //Contenu complet du JSON
+    console.log(data); //!Contenu complet du JSON
     const photographersData = data.photographers;
-    console.log(photographersData); //Affiche la partie photographers du JSON
-    //Récupération de l'ID de la page
-    const queryString_url_id = window.location.search;
-    const urlSearchParams = new URLSearchParams(queryString_url_id);
-    const id = urlSearchParams.get("id");
-    console.log(id); //Affiche l'id du photographe selectionné (243)
+    console.log(photographersData); //!Affiche la partie photographers du JSON
+
     //Affiche la liste par ID
     const sortedById = {}; 
     for (let i = 0, len = photographersData.length; i < len; i++) { 
         sortedById[photographersData[i].id] = photographersData[i]; }
-        console.log(sortedById); 
-        console.log(sortedById[id]); //Affiche l'objet avec l'id de la page
-let photographersArray = data.photographers; //Tableau des donnés des photographes
-let length = photographersArray.length; //Longueur du tableau
-//Boucle dans le tableau
-    for (let i = 0; i < length; i++){
-        //Affichage du tableau de données des photographes
-        console.log(photographersArray[i])}
         console.log({photographers:sortedById[id]})
-//Retourne le tableau photographers seulement une fois
-return ({photographers:sortedById[id]}) //retourne bien l'objet avec l'id correspondant
+//!retourne l'objet (le photographe) avec l'id correspondant au photographe//
+return ({photographers:sortedById[id]}) 
 }
+
+//!Affichage/positionnement de l'article
 //Affichage des données des photographes
     async function displayData(photographer) {
         //Selection de la photograph_header de l'index.html (section entière)
@@ -42,9 +40,44 @@ return ({photographers:sortedById[id]}) //retourne bien l'objet avec l'id corres
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
     }
+
+
+//!Récupération des données voulues (Profil des photographes)
+
+async function getMedia() {
+
+        //Récupération de l'ID de la page
+        const queryString_url_id = window.location.search;
+        const urlSearchParams = new URLSearchParams(queryString_url_id);
+        const id = urlSearchParams.get("id");
+        console.log(id); //Affiche l'id du photographe selectionné (243)
+    // Récupération des données du fichier JSON
+    const response = await fetch(json_url);
+    //Conversion des datas en JSON
+    const data = await response.json();
+    //Affichage du tableau des photographes
+   //Lien vers le fichier JSON
+    // console.log(data); //Contenu complet du JSON
+    const allData = data;
+    console.log(allData); //!Affiche la partie media ET photographers du JSON
+
+let mediaById = allData.media.filter( function(el) {
+    return el.photographerId == id;
+});
+console.log(mediaById); //Affiche toutes les images correspondants au photographe
+
+}
+
+
+//!A compléter une fois les fonctions du dessus valide
+//?Initialisation des toutes les fonctions
 async function init() {
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
+        // const { media } = await getMedia();
         await displayData(photographers);
+        // await displayData(media);
     }
     init(); //RETOURNE UNE PROMESSE EN ATTENTE
+
+    getMedia();
