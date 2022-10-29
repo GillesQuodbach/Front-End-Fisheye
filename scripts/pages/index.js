190e9f65d6fe7701860d9710c99b0107062f3024
Gilles Quodbach <quodbach.gilles@gmail.com>
2022-10-29 19:26:17 +0200

    Corrections

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -19,12 +19,6 @@ async function displayData (photographers) {
     const userCardDOM = photographerModel.getUserCardDOM()
     photographersSection.appendChild(userCardDOM)
   })
-  // const cardsLinks = document.querySelectorAll('.cards-container')
-  // cardsLinks.forEach((cardsLink) =>
-  //   cardsLink.addEventListener('click', () => {
-  //     window.location = `photographer.html?id=${cardsLink.id}`
-  //   })
-  // )
     }
     async function init () {
       const { photographers } = await getPhotographers()
